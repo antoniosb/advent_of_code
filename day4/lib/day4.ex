3cd871d4dc74f8344b43552fc0ec11c336e75159
Antonio Britto <antonioabritto@gmail.com>
2018-12-09 17:44:21 -0200

    add NimbleParsec as a dependency and parse input with it

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -1,2 +1,29 @@
 defmodule Day4 do
+  import NimbleParsec
+
+  def parse_log(input) when is_binary(input) do
+    {:ok, [year, month, day, hour, minute, id], "", _, _, _} = parsec_log(input)
+
+    {{year, month, day}, hour, minute, id}
+  end
+
+  defparsecp(
+    :parsec_log,
+    ignore(string("["))
+    |> integer(4)
+    |> ignore(string("-"))
+    |> integer(2)
+    |> ignore(string("-"))
+    |> integer(2)
+    |> ignore(string(" "))
+    |> integer(2)
+    |> ignore(string(":"))
+    |> integer(2)
+    |> ignore(string("] "))
+    |> choice([
+      ignore(string("Guard #")) |> integer(min: 1) |> ignore(string(" begins shift")),
+      string("falls asleep") |> replace(:down),
+      string("wakes up") |> replace(:up)
+    ])
+  )
 end
--- day4/test/day4_test.exs
+++ day4/test/day4_test.exs
@@ -1,4 +1,13 @@
 defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
+
+  test "parses the input" do
+    assert Day4.parse_log("[1518-11-01 00:00] Guard #10 begins shift") ==
+             {{1518, 11, 01}, 00, 00, 10}
+
+    assert Day4.parse_log("[1518-11-01 00:05] falls asleep") == {{1518, 11, 01}, 00, 05, :down}
+
+    assert Day4.parse_log("[1518-11-01 00:25] wakes up") == {{1518, 11, 01}, 00, 25, :up}
+  end
 end
