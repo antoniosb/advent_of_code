8cf6fac0374c8a4b5f76c8cd553a4355a18e0e8d
Antonio Britto <antonioabritto@gmail.com>
2018-12-09 15:35:06 -0200

    trying to work with erlang :ets

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -28,21 +28,31 @@ defmodule Day3 do
   ...>  [2, 3, 1, 4, 4],
   ...>  [3, 5, 5, 2, 2],
   ...> ])
-  iex> claimed[{4,2}]
+  iex> :ets.lookup_element(claimed, {4,2}, 2)
   [2]
-  iex> claimed[{4,4}]
-  [2, 1]
+  iex> :ets.lookup_element(claimed, {4,4}, 2) |> Enum.sort()
+  [1, 2]
 
   """
-  @spec claimed_inches([parsed_claim]) :: %{coordinate => [id]}
+  @spec claimed_inches([parsed_claim]) :: :ets.tab()
   def claimed_inches(parsed_claims) do
-    Enum.reduce(parsed_claims, %{}, fn [id, left, top, width, height], acc ->
-      Enum.reduce((left + 1)..(left + width), acc, fn x, acc ->
-        Enum.reduce((top + 1)..(top + height), acc, fn y, acc ->
-          Map.update(acc, {x, y}, [id], &[id | &1])
-        end)
-      end)
-    end)
+    table = :ets.new(:claimed_inches, [:duplicate_bag, :private])
+
+    for [id, left, top, width, height] <- parsed_claims,
+        x <- (left + 1)..(left + width),
+        y <- (top + 1)..(top + height) do
+      :ets.insert(table, {{x, y}, id})
+    end
+
+    table
+
+    # Enum.reduce(parsed_claims, %{}, fn [id, left, top, width, height], acc ->
+    #   Enum.reduce((left + 1)..(left + width), acc, fn x, acc ->
+    #     Enum.reduce((top + 1)..(top + height), acc, fn y, acc ->
+    #       Map.update(acc, {x, y}, [id], &[id | &1])
+    #     end)
+    #   end)
+    # end)
   end
 
   @doc """
@@ -57,10 +67,14 @@ defmodule Day3 do
     ...> ]) |> Enum.sort()
     [{4,4}, {4,5}, {5,4}, {5,5}]
   """
-  @spec overlapped_inches([parsed_claim]) :: [coordinate]
-  def overlapped_inches(parsed_claims) do
-    for {coordinate, [_, _ | _]} <- claimed_inches(parsed_claims), do: coordinate
-  end
+  # @spec overlapped_inches([parsed_claim]) :: [coordinate]
+  # def overlapped_inches(parsed_claims) do
+  #   :ets.select(claimed_inches(parsed_claims), [
+  #     {{:"$1", :"$2"}, [{:>, {:length, :"$2"}, 1}], [:"$1"]}
+  #   ])
+
+  #   # for {coordinate, [_, _ | _]} <- claimed_inches(parsed_claims), do: coordinate
+  # end
 
   @doc """
     Find non overlapping claim
@@ -84,7 +98,8 @@ defmodule Day3 do
       Enum.find(parsed_claims, fn [id, left, top, width, height] ->
         Enum.all?((left + 1)..(left + width), fn x ->
           Enum.all?((top + 1)..(top + height), fn y ->
-            Map.get(claimed_inches, {x, y}) == [id]
+            # Map.get(claimed_inches, {x, y}) == [id]
+            :ets.lookup_element(claimed_inches, {x, y}, 2) == [id]
           end)
         end)
       end)
