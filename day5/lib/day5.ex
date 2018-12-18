19acb0f279f6941a0afb14f59d639153d281a0a8
Antonio Britto <antonioabritto@gmail.com>
2018-12-18 19:03:39 -0200

    resolve part two

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -1,5 +1,5 @@
 defmodule Day5 do
-  @moduledoc """
+  @doc """
 
     Examples:
       iex> Day5.react("dabAcCaCBAcCcaDA")
@@ -9,14 +9,42 @@ defmodule Day5 do
       "aabAAB"
   """
   def react(polymer) when is_binary(polymer),
-    do: react(polymer, [])
+    do: react_and_discard(polymer, [], nil, nil)
 
-  def react(<<letter1, rest::binary>>, [letter2 | acc]) when abs(letter1 - letter2) == 32,
-    do: react(rest, acc)
+  @doc """
 
-  def react(<<letter1, rest::binary>>, acc),
-    do: react(rest, [letter1 | acc])
+    Examples:
+      iex> Day5.react_and_discard("dabAcCaCBAcCcaDA", ?A, ?a)
+      "dbCBcD"
+  """
+  def react_and_discard(polymer, letter1, letter2) when is_binary(polymer),
+    do: react_and_discard(polymer, [], letter1, letter2)
 
-  def react(<<>>, acc),
+  def react_and_discard(<<letter1, rest::binary>>, acc, discard1, discard2)
+      when letter1 == discard1 or letter1 == discard2,
+      do: react_and_discard(rest, acc, discard1, discard2)
+
+  def react_and_discard(<<letter1, rest::binary>>, [letter2 | acc], discard1, discard2)
+      when abs(letter1 - letter2) == 32,
+      do: react_and_discard(rest, acc, discard1, discard2)
+
+  def react_and_discard(<<letter1, rest::binary>>, acc, discard1, discard2),
+    do: react_and_discard(rest, [letter1 | acc], discard1, discard2)
+
+  def react_and_discard(<<>>, acc, _discard1, _discard2),
     do: acc |> Enum.reverse() |> List.to_string()
+
+  @doc """
+    Examples:
+      iex> Day5.find_problematic_unit("dabAcCaCBAcCcaDA")
+      {?C, 4}
+  """
+  def find_problematic_unit(polymer) do
+    letter_and_length =
+      for letter <- ?A..?Z do
+        {letter, byte_size(react_and_discard(polymer, letter, letter + 32))}
+      end
+
+    Enum.min_by(letter_and_length, &elem(&1, 1))
+  end
 end
