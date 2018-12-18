5197ddf4776e6117a104b4308f4b33407cbd9643
Antonio Britto <antonioabritto@gmail.com>
2018-12-18 18:07:56 -0200

    resolve part one with binary pattern matching

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -1,2 +1,22 @@
 defmodule Day5 do
+  @moduledoc """
+
+    Examples:
+      iex> Day5.react("dabAcCaCBAcCcaDA")
+      "dabCBAcaDA"
+
+      iex> Day5.react("aabAAB")
+      "aabAAB"
+  """
+  def react(polymer) when is_binary(polymer),
+    do: react(polymer, [])
+
+  def react(<<letter1, rest::binary>>, [letter2 | acc]) when abs(letter1 - letter2) == 32,
+    do: react(rest, acc)
+
+  def react(<<letter1, rest::binary>>, acc),
+    do: react(rest, [letter1 | acc])
+
+  def react(<<>>, acc),
+    do: acc |> Enum.reverse() |> List.to_string()
 end
